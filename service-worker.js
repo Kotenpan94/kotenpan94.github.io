f83a066951dac2cd766723fd8084af7d79c339c1
Kotenpan94 <59397435+Kotenpan94@users.noreply.github.com>
2020-01-03 22:43:27 -0500

    Add files via upload
    More updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.327f68d46c9399f29b8f9895fc0868f5.js"
+  "/precache-manifest.5e09be6ddeb65b31f5cfcfefb07d7d7b.js"
 );
 
 self.addEventListener('message', (event) => {
